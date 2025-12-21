a6687614c34bdbc4b1cd20270cc9d06619eff77c
stickia <windowssucks@notreal.com>
2025-12-21 13:46:51 -0500

    Removed comments

--- cpp/src/main/headers/libtest.h
+++ cpp/src/main/headers/libtest.h
@@ -1,10 +1,8 @@
 #ifndef libtest_h
 
-// Define MATHUTILS_H to prevent multiple inclusions
 #define libtest_h
 
 // Function declarations (only the function names and parameters, not the logic)
 extern "C" int add(int a, int b);
-//extern "C" int add(int a, int b);
 
 #endif
